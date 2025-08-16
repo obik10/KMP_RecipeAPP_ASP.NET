399b5b7f57fcb2e4380089f96c77fa8cddb61a13
robbyulisa322@gmail.com <robbyulisa322@gmail.com>
2025-08-16 17:17:20 +0700

    Phase 4 Finalization - Checkpoint

---
+++ Backend/src/RecipeApp.Infrastructure/Persistence/Migrations/20250816101156_AddExternalFieldsToRecipe.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeApp.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExternalFieldsToRecipe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalId",
+                table: "Recipes",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalSource",
+                table: "Recipes",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsExternal",
+                table: "Recipes",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalSource",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "IsExternal",
+                table: "Recipes");
+        }
+    }
+}
